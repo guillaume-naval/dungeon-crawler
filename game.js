63e140252dba8714f8108c4d61cda6a0a6edbf81
Guillaume NAVAL <navalguillaume@gmail.com>
2021-12-08 09:52:37 +0100

    assets tiles

--- game.js
+++ game.js
@@ -1,32 +1,67 @@
 kaboom({
-    global : true,
-    scale:1,
-    debug:true,
-})
+  global: true,
+  scale: 3,
+  debug: true,
+  clearColor: [0, 0, 0],
+});
 
-loadSpriteAtlas('/assets/walkingtunic1.png', {
-    'link':{
-        'x':0,
-        'y':0,
-        'width':960,
-        'height':128,
-        'sliceX':32,
-        'sliceY':32,
-        'anims':{
-            'walk-right': { from: 0, to:29, loop:true, speed:15},
-            'walk-up':{ from: 30, to:59, loop:true, speed:15},
-            'walk-left':{ from: 60, to:99, loop:true, speed:15},
-            'walk-down':{ from: 100, to:129, loop:true, speed:15},
-            'idle-down':{from : 100, to : 100},
-            'idle-side':{from:60, to:60},
-            'idle-up':{from : 30,to:30},
-        }
+loadSpriteAtlas("/assets/sprites/hero/walkingtunic1.png", {
+  link: {
+    x: 0,
+    y: 0,
+    width: 960,
+    height: 128,
+    sliceX: 30,
+    sliceY: 4,
+    anims: {
+      "walk-right": { from: 0, to: 29, loop: true, speed: 30 },
+      "walk-up": { from: 30, to: 59, loop: true, speed: 30 },
+      "walk-left": { from: 60, to: 89, loop: true, speed: 30 },
+      "walk-down": { from: 100, to: 119, loop: true, speed: 30 },
+      "idle-down": { from: 100, to: 100 },
+      "idle-left": { from: 60, to: 60 },
+      "idle-right": { from: 0, to: 0 },
+      "idle-up": { from: 30, to: 30 },
+    },
+  },
+});
+
+function CharacterMovement() {
+  const player = add([sprite("link"), pos(center())]);
+  player.play("idle-up");
+  player.action(() => {
+    const left = keyIsDown("left");
+    const right = keyIsDown("right");
+    const up = keyIsDown("up");
+    const down = keyIsDown("down");
+    const speed = 1;
+    const curAnim = player.curAnim();
+
+    if (left) {
+      if (curAnim !== "walk-left") {
+        player.play("walk-left");
+      }
+      player.pos.x -= speed;
+    } else if (right) {
+      if (curAnim !== "walk-right") {
+        player.play("walk-right");
+      }
+      player.pos.x += speed;
+    } else if (up) {
+      if (curAnim !== "walk-up") {
+        player.play("walk-up");
+      }
+      player.pos.y -= speed;
+    } else if (down) {
+      if (curAnim !== "walk-down") {
+        player.play("walk-down");
+      }
+      player.pos.y += speed;
+    } else {
+      const direction = curAnim.split("-").pop() ?? "down";
+      player.play("idle-" + direction);
     }
-})
+  });
+}
 
-const player = add([
-    sprite("link"),
-    pos(width()*0.5,height()*0.5),
-])
-
-player.play("walk-down")
+CharacterMovement();
